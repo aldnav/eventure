7cb28b143c45f68c3d737e1ab9f7ba3a0fe71b9d
Aldrin Navarro <aldrinnavarro16@gmail.com>
2017-07-05 02:59:21 +0800

    Make models guided by Atom Activity Spec

--- activity/models.py
+++ activity/models.py
@@ -1,6 +1,50 @@
 # -*- coding: utf-8 -*-
+"""
+Aims to follow this spec: http://activitystrea.ms/specs/atom/1.0/
+"""
+
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
-# Create your models here.
+
+class Activity(models.Model):
+    """An activity consists of an actor, a verb and an object."""
+    # Actor.
+    actor_type = models.ForeignKey(ContentType, on_delete=models.CASCADE,
+                                   related_name='activities_actor')
+    actor_id = models.PositiveIntegerField()
+    actor = GenericForeignKey('actor_type', 'actor_id')
+
+    # Object
+    object_type = models.ForeignKey(ContentType, on_delete=models.CASCADE,
+                                    related_name='activities')
+    object_id = models.PositiveIntegerField()
+    object_ref = GenericForeignKey('object_type', 'object_id')
+
+    verb = models.ForeignKey('Verb', on_delete=models.CASCADE,
+                             related_name='activities')
+
+    # Target
+    # "Wendy saved a song to her playlist", the target of the activity is
+    # "playlist"
+    target_type = models.ForeignKey(ContentType, on_delete=models.CASCADE,
+                                    related_name='activities_target',
+                                    null=True, blank=True)
+    target_id = models.PositiveIntegerField(null=True, blank=True)
+    target = GenericForeignKey('target_type', 'target_id')
+
+    published = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255, blank=True)
+    summary = models.CharField(max_length=255, blank=True)
+
+
+class Verb(models.Model):
+    """The Object of the Activity Construct """
+    name = models.CharField(max_length=64, unique=True)
+    # @NOTE: Handle tenses as properties
+
+    def __unicode__(self):
+        return self.name
--- activity/tests.py
+++ activity/tests.py
@@ -2,5 +2,47 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from .models import Activity, Verb
+from extra.models import Game, Event, Post, Video
+
+
+class ActivityModelTestCase(TestCase):
+
+    def setUp(self):
+        self.user1 = User.objects.create_user(username='Gilfoyle', password='pass')
+        self.user2 = User.objects.create_user(username='Dinesh', password='pass')
+        self.game = Game.objects.create(title='Super Mario Kart')
+        self.event = Event.objects.create(title='Book Signing')
+        self.post = Post.objects.create(title='I Have a Dream')
+        self.video = Video.objects.create(title='All Time Top 10')
+
+    def test_implicit_activity(self):
+        """Tests activity with no target"""
+        play = Verb.objects.create(name='plays')
+        activity = Activity.objects.create(
+            actor=self.user1,
+            object_ref=self.game,
+            verb=play,
+            title='{} {} {}'.format(self.user1, play, self.game)
+        )
+        print activity.title
+        self.assertEqual(
+            activity.title, '{} {} {}'.format(self.user1, play, self.game))
+
+    def test_explicit_activity(self):
+        """Tests activity with target"""
+        mention = Verb.objects.create(name='mention')
+        activity = Activity.objects.create(
+            actor=self.user2,
+            object_ref=self.user1,
+            verb=mention,
+            target=self.video,
+            title='{} {} {} on {}'.format(
+                self.user2, mention, self.user1, self.video)
+        )
+        print activity.title
+        self.assertEqual(
+            activity.title, '{} {} {} on {}'.format(
+                self.user2, mention, self.user1, self.video))
