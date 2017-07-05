d9c2f6f918ce069fc68fb7bca3230247daa3c0fb
Aldrin Navarro <aldrinnavarro16@gmail.com>
2017-07-05 16:57:00 +0800

    Added tests

--- extra/tests.py
+++ extra/tests.py
@@ -1,6 +1,31 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
+from django.core import serializers
 from django.test import TestCase
 
-# Create your tests here.
+from .views import ActivitySerializer
+from .models import Post
+from activity.models import Activity, Verb
+
+
+class SerializerTestCase(TestCase):
+
+    def setUp(self):
+        self.user1 = User.objects.create_user(username='Gilfoyle', password='pass')
+        self.user2 = User.objects.create_user(username='Dinesh', password='pass')
+        self.post = Post.objects.create(title='I Have a Dream')
+
+    def test_serialize(self):
+        like = Verb.objects.create(name='like')
+        action = Activity.objects.create(
+            actor=self.user1,
+            verb=like,
+            object_ref=self.post,
+        )
+        serializer = ActivitySerializer()
+        serialized = serializer.serialize(action, ['pk', 'published'])
+        self.assertIsNotNone(serialized)
+        self.assertEquals(serialized['pk'], action.pk)
+        # data = serializers.serialize('json', [action])
